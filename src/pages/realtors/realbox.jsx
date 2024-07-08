3f250a241ffdf65d8acb295d1770ca635d7f8f0b
FardzanaBegum <fardzana19@outlook.com>
2024-07-08 17:08:20 +0530

    update

---
+++ src/pages/realtors/realbox.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const realbox = () => {
+  return (
+    <div>realbox</div>
+  )
+}
---
+++ src/pages/realtors/realpage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const realpage = () => {
+  return (
+    <div>realpage</div>
+  )
+}
